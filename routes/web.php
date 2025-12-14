63ab3f128e86710b6db04c99ad6829de7620f574
diegobg190 <diegobg190@gmail.com>
2025-12-14 02:43:19 -0600

    fix: eliminar enlace a ruta de productos inexistente en POS

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SaleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
+// Rutas para POS (Punto de Venta)
+Route::get('/sales/pos', [SaleController::class, 'pos'])->name('sales.pos');
+Route::get('/sales/search', [SaleController::class, 'searchProducts'])->name('sales.search');
