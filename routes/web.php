bb37a7b4a67646fbd9d21ee7660c7ba219a780c0
diegobg190 <diegobg190@gmail.com>
2025-12-14 16:43:30 -0600

    fix: restaurar rutas de productos tras merge

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SaleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Rutas para Productos
+Route::resource('products', ProductController::class);
 
 // Rutas para POS (Punto de Venta)
 Route::get('/sales/pos', [SaleController::class, 'pos'])->name('sales.pos');
